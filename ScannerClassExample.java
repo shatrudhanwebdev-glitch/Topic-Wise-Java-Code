25629ab6777e48c68e34c64aaa2a5ab019cb767d
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-12 15:47:26 +0530

    Scanner Class Example

---
+++ ScannerClassExample.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ScannerClassExample {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+
+        System.out.println("Welcome " + name);
+    }
+}
