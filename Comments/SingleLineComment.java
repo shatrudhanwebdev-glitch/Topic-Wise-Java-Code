5eb96cd63a48ca63650e19c75e845ab1cbe2aba2
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-14 17:05:01 +0530

    yes

---
+++ Comments/SingleLineComment.java
@@ -0,0 +1,11 @@
+package Comments;
+
+public class SingleLineComment {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        // int num1 = 52; //This is single line comment
+    }
+}
