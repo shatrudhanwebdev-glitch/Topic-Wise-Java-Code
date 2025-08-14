25a9f464b349db8e65eef6aac761d2f07027a614
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-14 22:28:25 +0530

    yes

---
+++ ScannerClassExample.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ScannerClassExample {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st number : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter 2nd number : ");
+        int num2 = sc.nextInt();
+
+        int mult = num1 * num2;
+
+        System.out.println("Multiple of num1 and num2 = " + mult);
+
+    }
+
+}
