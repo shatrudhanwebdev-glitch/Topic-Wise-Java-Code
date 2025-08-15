566d3ca539b342427c5a8a689414bc9d3ae5288d
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-15 22:53:15 +0530

    yes

---
+++ Operators in java/UnaryOperators.java
@@ -0,0 +1,27 @@
+public class UnaryOperators {
+
+    // shatrudha.web.developer
+
+    /*
+     * Unary operators only two types
+     * (a) Increment operetor
+     * 1. Pre-Increment (++i)
+     * 2. Post-Increment (i++)
+     * 
+     * (b) Decrement operator
+     * 1. Pre-Decrement (--i)
+     * 2. Post-Decrement (i--)
+     */
+
+    public static void main(String[] args) {
+
+        // Increment Operators
+        int x = 10;
+        System.out.println(++x); // For pre-increment
+        System.out.println(x++); // For post-increment
+
+        // Decrement Operators
+        System.out.println(--x); // For pre-decrement
+        System.out.println(x--); // For post-decrement
+    }
+}
