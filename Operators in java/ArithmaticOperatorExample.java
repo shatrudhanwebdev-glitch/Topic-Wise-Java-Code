4e9e0a76ca460e8ccf46277792837f2eb6a9877e
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-16 21:31:57 +0530

    yes

---
+++ Operators in java/ArithmaticOperatorExample.java
@@ -0,0 +1,32 @@
+public class ArithmaticOperatorExample {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        /*
+         * Arithmetic Operators
+         * 1. addition
+         * 2. subtraction
+         * 3. multiplication
+         * 4. division
+         * 5. modlus (qoutient)
+         */
+
+        int x = 50, y = 5;
+        int add, sub, mul, div, mod;
+
+        add = x + y;
+        sub = x - y;
+        mul = x * y;
+        mod = x % y;
+        div = x / y;
+
+        // Output
+        System.out.println("Addition = " + add);
+        System.out.println("Subtraction = " + sub);
+        System.out.println("Multiplication = " + mul);
+        System.out.println("Division = " + div);
+        System.out.println("Modlus = " + mod);
+    }
+}
