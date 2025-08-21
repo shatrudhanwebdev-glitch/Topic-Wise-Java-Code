2442d95d806bda76dec9f9b423e335b0367edfc6
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-21 13:13:00 +0530

    yes

---
+++ Array/AccessElement.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class AccessElement {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        // Access element from an array
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
---
+++ CommanLineArgument/JavaCommandLineArgumentExample.java
@@ -0,0 +1,13 @@
+package CommanLineArgument;
+
+public class JavaCommandLineArgumentExample {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
