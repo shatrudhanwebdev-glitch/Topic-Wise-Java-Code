078e6593dacf1ff4bcda0b7dd1d923b4d701fa7b
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-21 13:40:57 +0530

    yes

--- Array/AccessElement.java
+++ Array/AccessElement.java
@@ -10,7 +10,7 @@ public class AccessElement {
 
         int[] arr = { 10, 20, 30, 40, 50 };
 
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
 
         }
