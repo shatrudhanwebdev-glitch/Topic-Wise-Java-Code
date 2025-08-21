b697e1d22b7a06b15c97e03b7da869005c4ed442
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-21 17:32:37 +0530

    yes

---
+++ Array/Types of array/One Dimensional (1-D)/OneDimensional.java
@@ -0,0 +1,24 @@
+public class OneDimensional {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        int arr[] = { 2, 4, 6, 8, 10 };
+
+        // is tarh bhi likh sakta hu
+        /*
+         * int [] = i
+         * 
+         * arr[0] = 2;
+         * arr[1] = 4;
+         * arr[2] = 6;
+         * arr[3] = 8;
+         * arra[4] = 10;
+         */
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Array/Types of array/One Dimensional (1-D)/Questions/Ex1.java
@@ -0,0 +1,19 @@
+public class Ex1 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        // WAP suf of array using for loop.
+
+        int arr[] = { 10, 20, 30, 40, 50 };
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = arr[1];
+
+        }
+        System.out.println(sum + sum);
+
+    }
+}
