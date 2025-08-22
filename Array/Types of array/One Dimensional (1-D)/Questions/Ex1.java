4c90a0828bb84ac473a2526fad18b5135ce8f5d8
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-22 19:23:28 +0530

    yes

--- Array/Types of array/One Dimensional (1-D)/Questions/Ex1.java
+++ Array/Types of array/One Dimensional (1-D)/Questions/Ex1.java
@@ -4,14 +4,13 @@ public class Ex1 {
 
     public static void main(String[] args) {
 
-        // WAP suf of array using for loop.
+        // WAP sum of array using for loop.
 
         int arr[] = { 10, 20, 30, 40, 50 };
         int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
             sum = arr[1];
-
         }
         System.out.println(sum + sum);
 
