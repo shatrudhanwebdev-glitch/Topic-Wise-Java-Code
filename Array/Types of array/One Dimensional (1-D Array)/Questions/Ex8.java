6c732f00da5eb091ae3c3371fc2f42be3803d44d
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-08-25 23:27:09 +0530

    yes

--- Array/Types of array/One Dimensional (1-D Array)/Questions/Ex8.java
+++ Array/Types of array/One Dimensional (1-D Array)/Questions/Ex8.java
@@ -1,12 +1,13 @@
 public class Ex8 {
 
-    // shatrudha.web.developer
+    // shatrudhan..web.developer
 
     public static void main(String[] args) {
 
         // Question - Count how many even and odd numbers are present in the array.
 
         int arr[] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
         int evencount = 0, oddcount = 0;
 
         for (int i = 0; i < arr.length; i++) {
