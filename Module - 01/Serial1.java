baf413f246ed5d0c0cb12ecb08b7378ae384bf5a
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2025-09-07 13:18:36 +0530

    yes

---
+++ Module - 01/Serial1.java
@@ -0,0 +1,18 @@
+public class Serial1 {
+
+    // shatrudhan.web.developer
+
+    public static void main(String[] args) {
+
+        int mark = 75;
+        if (mark >= 75) {
+            System.out.println("1st div");
+        } else if (mark < 75 && mark >= 50) {
+            System.out.println("nd div");
+        } else if (mark < 50 && mark >= 30) {
+            System.out.println("3rd div ");
+        } else {
+            System.out.println("3rd div ");
+        }
+    }
+}
